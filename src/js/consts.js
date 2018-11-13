e69f511261abb0f6419b34f3cc542fab3fcbe062
c0ban-project <39722780+c0ban-project@users.noreply.github.com>
2018-11-13 14:15:18 +0900

    Change unit name "OKM" -> "XCB"

--- src/js/consts.js
+++ src/js/consts.js
@@ -260,7 +260,7 @@ var APPROX_SECONDS_PER_BLOCK = USE_TESTNET ? 20 * 60 : 60; //a *rough* estimate 
 
 var KEY_ASSET = {
   'BTC': 'RYO',
-  'XCP': 'OKM',
+  'XCP': 'XCB',
   'USD': 'JPY',
   'Bitcoin': 'c0ban',
   'Counterparty': 'c0ban-party'
